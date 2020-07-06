65b91c4003a3c9981fdd1716ff6e69080fb1f54f
safath07 <52937124+safath07@users.noreply.github.com>
2020-07-07 10:29:54 +1200

    Update AboutFragment.java

--- AboutFragment.java
+++ AboutFragment.java
@@ -14,5 +14,6 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_about, container, false);
+        //Toast me
     }
 }
